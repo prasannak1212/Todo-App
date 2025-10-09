3890a3cc4caedf50281d28899fc20507a3ce1519
prasannak1212 <168375421+prasannak1212@users.noreply.github.com>
2025-10-10 00:42:21 +0530

    modified cors origin

--- Backend/main.py
+++ Backend/main.py
@@ -8,7 +8,7 @@ app.include_router(task_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # or ["http://localhost:5173"] if using Vite
+    allow_origins=["*"],  # or ["http://localhost:5173"] if using Vite
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
